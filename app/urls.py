113b9078707ba2a1a981ecab11e7a646638e7a12
Jayant Raj <jayantraj66041@gmail.com>
2023-02-01 14:41:29 +0530

    login, signup work

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from app.views import Landing
+from app.views import Landing, LogIn, Dashboard
 
 
 urlpatterns = [
     path("", Landing.as_view(), name="landing-page"),
+    path("login/", LogIn.as_view(), name="login"),
+
+    path("dashboard/", Dashboard.as_view(), name="dashboard"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -9,4 +11,45 @@ class Landing(View):
         return render(request, "landing.html")
 
     def post(self, request):
-        pass
+        fname = request.POST.get("fname")
+        lname = request.POST.get("lname")
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        re_password = request.POST.get("re-password")
+        
+        if password == re_password:
+            user = User()
+            user.first_name = fname
+            user.last_name = lname
+            user.username = username
+            user.email = email
+            user.set_password(password)
+            user.is_superuser = False
+            user.is_staff = False
+            user.is_active = True
+            user.save()
+
+            login(request, user)
+            return redirect("dashboard")
+
+        return redirect("landing-page")
+
+
+class LogIn(View):
+    def post(self, request):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect("dashboard")
+
+        return redirect("landing-page")
+    
+
+class Dashboard(View):
+    def get(self, request):
+        return render(request, "dashboard.html")
