60bd06030415d5e0957f91734fb64a6655c42824
Jayant Raj <jayantraj66041@gmail.com>
2023-02-04 11:10:53 +0530

    signup login validation

--- app/views.py
+++ app/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
-
 class Landing(View):
+
     def get(self, request):
+        if request.user.is_authenticated:
+            return redirect("dashboard")
+        
         return render(request, "landing.html")
 
     def post(self, request):
@@ -18,7 +22,7 @@ class Landing(View):
         password = request.POST.get("password")
         re_password = request.POST.get("re-password")
         
-        if password == re_password:
+        if password == re_password and username and email and password and fname:
             user = User()
             user.first_name = fname
             user.last_name = lname
@@ -37,6 +41,7 @@ class Landing(View):
 
 
 class LogIn(View):
+
     def post(self, request):
         username = request.POST.get("username")
         password = request.POST.get("password")
@@ -47,9 +52,22 @@ class LogIn(View):
             login(request, user)
             return redirect("dashboard")
 
+        messages.error(request, "<strong>Login Failed!</strong> You should check your Username or Password.")
+
         return redirect("landing-page")
     
 
 class Dashboard(View):
+
     def get(self, request):
-        return render(request, "dashboard.html")
+        if not request.user.is_authenticated:
+            return redirect("landing-page")
+        
+        return render(request, "dashboard.html", {
+            'user': User.objects.get(username=request.user)
+        })
+    
+    def post(self, request):
+        logout(request)
+        messages.success(request, "<strong>Successfully logout!</strong> Hope you'll visit here soon.")
+        return redirect("landing-page")
